8cafc5fcc35c2e162cfb956d91a9967326fc1938
Jeff Magee <jeffreymagee@gmail.com>
2019-10-24 11:05:43 +0100

    update

--- tinypulseppg.ino
+++ tinypulseppg.ino
@@ -20,6 +20,11 @@ MAFilter bpm;
 
 #define LED 1
 
+static const uint8_t heart_bits[] PROGMEM = { 0x00, 0x00, 0x38, 0x38, 0x7c, 0x7c, 0xfe, 0xfe, 0xfe, 0xff, 
+                                        0xfe, 0xff, 0xfc, 0x7f, 0xf8, 0x3f, 0xf0, 0x1f, 0xe0, 0x0f,
+                                        0xc0, 0x07, 0x80, 0x03, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 
+                                        0x00, 0x00 };
+
 //spo2_table is approximated as  -45.060*ratioAverage* ratioAverage + 30.354 *ratioAverage + 94.845 ;
 const uint8_t spo2_table[184] PROGMEM =
         { 95, 95, 95, 96, 96, 96, 97, 97, 97, 97, 97, 98, 98, 98, 98, 98, 99, 99, 99, 99, 
@@ -53,7 +58,7 @@ void print_digit(int x, int y, long val, char c=' ', uint8_t field = 3,
 /*
  *   Record, scale  and display PPG Wavefoem
  */
-const uint8_t MAXWAVE = 80;
+const uint8_t MAXWAVE = 72;
 
 class Waveform {
   public:
@@ -83,16 +88,16 @@ class Waveform {
         }
       }
 
-      void draw() {
+      void draw(uint8_t X) {
        for (int i=0; i<MAXWAVE; i++) {
         uint8_t y = disp_wave[i];
-        oled.drawPixel(i, y);
+        oled.drawPixel(X+i, y);
         if (i<MAXWAVE-1) {
           uint8_t nexty = disp_wave[i+1];
           if (nexty>y) {
-            for (uint8_t iy = y+1; iy<nexty; ++iy)  oled.drawPixel(i, iy);
+            for (uint8_t iy = y+1; iy<nexty; ++iy)  oled.drawPixel(X+i, iy);
           } else if (nexty<y) {
-            for (uint8_t iy = nexty+1; iy<y; ++iy)  oled.drawPixel(i, iy);
+            for (uint8_t iy = nexty+1; iy<y; ++iy)  oled.drawPixel(X+i, iy);
           }
         }
       }  
@@ -122,16 +127,17 @@ void draw_oled(int msg) {
                  oled.drawChar(118,0,'V');
                  break;
         case 2:  print_digit(86,0,beatAvg);
-                 wave.draw();
+                 wave.draw(8);
                  print_digit(98,16,SPO2f,' ',3,1);
                  oled.drawChar(116,16,'%');
                  print_digit(98,24,SPO2,' ',3,1);
                  oled.drawChar(116,24,'%');
                  break;
-        case 3:  oled.drawStr(10,12,F("Heart Rate"),1);
+        case 3:  oled.drawStr(28,12,F("tinyPulsePPG"),1);
+                 oled.drawXBMP(6,8,16,16,heart_bits);
+                 oled.drawXBMP(106,8,16,16,heart_bits);
                  break;
-        case 4:  oled.drawStr(10,12,F("Detecting..."),1);
-                 break;
+
         }
     } while (oled.nextPage());
 }
@@ -141,7 +147,7 @@ void setup(void) {
   oled.init();
   oled.fill(0x00);
   draw_oled(3);
-  delay(2000);
+  delay(3000);
   if (!sensor.begin())  {
     draw_oled(0);
     while (1);
@@ -152,56 +158,52 @@ void setup(void) {
 long lastBeat = 0;    //Time of the last beat 
 long displaytime = 0; //Time of the last display update
 bool led_on = false;
-bool fingerdown = false;
 
 void loop()  {
     sensor.check();
     long now = millis();   //start time of this cycle
-    if (sensor.available()) {
-      uint32_t irValue = sensor.getIR(); 
-      uint32_t redValue = sensor.getRed();
-      sensor.nextSample();
-      // finger down not detected display message
-      fingerdown = (irValue >= 7000);
-      if (!fingerdown) {
-          voltage = getVCC();
-          draw_oled(1); 
-          delay(100);
-          return;
-      } 
-      // remove DC element
-      int16_t IR_signal = pulseIR.dc_filter(irValue);
-      int16_t Red_signal = pulseRed.dc_filter(redValue);
-      // invert waveform to get classical BP waveshape
-      wave.record(-IR_signal); 
-      // check IR for heartbeat, Red is needed to compute avgRed
-      pulseRed.isBeat(pulseRed.ma_filter(Red_signal)); 
-      if (pulseIR.isBeat(pulseIR.ma_filter(IR_signal))){
-          long btpm = 60000/(now - lastBeat);
-          if (btpm > 0 && btpm < 200) beatAvg = bpm.filter((int16_t)btpm);
-          lastBeat = now; 
-          digitalWrite(LED, HIGH); 
-          led_on = true;
-          // compute SpO2 ratio
-          long numerator   = (pulseRed.avgAC() * pulseIR.avgDC())/256;
-          long denominator = (pulseRed.avgDC() * pulseIR.avgAC())/256;
-          int RX100 = (denominator>0) ? (numerator * 100)/denominator : 999;
-          // using formula
-          SPO2f = (10400 - RX100*17+50)/100;  
-          // from table
-          if ((RX100>=0) && (RX100<184))
-                SPO2 = pgm_read_byte_near(&spo2_table[RX100]);
-      }
-    }
-    // flash led for 25 ms
-    if (led_on && (now - lastBeat)>25){
-        digitalWrite(LED, LOW);
-        led_on = false;
-    }
-    // update display every 50 ms if fingerdown
-    if ((now-displaytime>50) && fingerdown) {
-        displaytime = now;
-        wave.scale();
-        draw_oled(2);
+    if (!sensor.available()) return;
+    uint32_t irValue = sensor.getIR(); 
+    uint32_t redValue = sensor.getRed();
+    sensor.nextSample();
+    if (irValue<7000) {
+        voltage = getVCC();
+        draw_oled(1); // finger not down message
+        delay(100);
+    } else {
+        // remove DC element
+        int16_t IR_signal = pulseIR.dc_filter(irValue);
+        int16_t Red_signal = pulseRed.dc_filter(redValue);
+        // invert waveform to get classical BP waveshape
+        wave.record(-IR_signal); 
+        // check IR for heartbeat, Red is needed to compute avgRed
+        pulseRed.isBeat(pulseRed.ma_filter(Red_signal)); 
+        if (pulseIR.isBeat(pulseIR.ma_filter(IR_signal))){
+            long btpm = 60000/(now - lastBeat);
+            if (btpm > 0 && btpm < 200) beatAvg = bpm.filter((int16_t)btpm);
+            lastBeat = now; 
+            digitalWrite(LED, HIGH); 
+            led_on = true;
+            // compute SpO2 ratio
+            long numerator   = (pulseRed.avgAC() * pulseIR.avgDC())/256;
+            long denominator = (pulseRed.avgDC() * pulseIR.avgAC())/256;
+            int RX100 = (denominator>0) ? (numerator * 100)/denominator : 999;
+            // using formula
+            SPO2f = (10400 - RX100*17+50)/100;  
+            // from table
+            if ((RX100>=0) && (RX100<184))
+              SPO2 = pgm_read_byte_near(&spo2_table[RX100]);
+        }
+        // flash led for 25 ms
+        if (led_on && (now - lastBeat)>25){
+            digitalWrite(LED, LOW);
+            led_on = false;
+        }
+        // update display every 50 ms if fingerdown
+        if (now-displaytime>50) {
+            displaytime = now;
+            wave.scale();
+            draw_oled(2);
+        }
     }
 }
